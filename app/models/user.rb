0257357a8ab1399c3b34be68f8a3ba6267f23641
Fellipe Castro <contact@fellipecastro.com>
2015-06-09 09:34:00 -0300

    Implement secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-  validates :full_name, :location, :password, :presence => true
+  validates :full_name, :location, :presence => true
   validates :email, :presence => true,
                     :format => { :with => EMAIL_REGEXP},
                     :uniqueness => true
-  validates_confirmation_of :password
   validates_length_of :bio, :minimum => 30, :allow_blank => false
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608022741) do
+ActiveRecord::Schema.define(version: 20150609122317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 20150608022741) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
