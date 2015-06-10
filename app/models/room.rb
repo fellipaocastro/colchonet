940233f82497fa2c3ad2a137e0fabc01cc8c4c35
Fellipe Castro <contact@fellipecastro.com>
2015-06-10 09:47:15 -0300

    Apply I18n

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ActiveRecord::Base
   def complete_name
     "#{title}, #{location}"
   end
+
+  validates :title, :location, :presence => true
+  validates_length_of :description, :minimum => 30, :allow_blank => false
 end
