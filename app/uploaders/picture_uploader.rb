2e2c0d9e66d188de9a8c796b114e61a9aac4be8f
Fellipe Castro <contact@fellipecastro.com>
2015-06-14 10:52:49 -0300

    Uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -25,7 +25,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [500, 500]
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
 
   # Create different versions of your uploaded files:
   version :thumb do
